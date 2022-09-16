f1f71f48f87f87472241ac8a24c78e2babe1fa03
aaaris <2997600742@qq.com>
2022-09-16 09:26:34 +0800

    update .gitignore

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  parallel: false,
-  publicPath: "/",
-  // assetsDir:'static',
-  transpileDependencies: true,
-  lintOnSave: false
-})
